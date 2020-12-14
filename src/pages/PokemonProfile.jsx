be9d0346e750d582a6b0cf1212cd4a02fb101fe7
= <=>
2020-12-14 21:10:53 +0700

    update react app v5

--- src/pages/PokemonProfile.jsx
+++ src/pages/PokemonProfile.jsx
@@ -54,7 +54,7 @@ const PokemonProfile = (props) => {
             while(check);
             addContext({id,name,image,nickName},'mypokemons');
             alert("Your new "+name+" nickname is "+nickName);
-            window.location.href = '/mypokemon/list';   
+            window.location.href = 'react-app-1/mypokemon/list';   
         }
     }
 
@@ -63,7 +63,7 @@ const PokemonProfile = (props) => {
         if(window.confirm("Are you sure want to release "+nickName+"?")){
             removeContext(nickName,'nickName','mypokemons');
             alert(nickName+" has been released");
-            window.location.href = '/mypokemon/list';
+            window.location.href = 'react-app-1/mypokemon/list';
         }
     }
 
